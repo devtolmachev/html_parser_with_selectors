40943cd31994177e1ccbbf9ce03aab9d5db015e6
dev.tolmachev <dev.tolmachev@gmail.com>
2025-03-18 13:34:46 +0700

    add auth header and updated request body format

--- html_parser_with_selectors/api.py
+++ html_parser_with_selectors/api.py
@@ -5,23 +5,31 @@ from concurrent.futures import ThreadPoolExecutor
 import uvicorn
 from fastapi import FastAPI, Request
 from loguru import logger
-
+from pydantic import BaseModel
 
 from html_parser_with_selectors.main import Parser
+from html_parser_with_selectors.secure import get_token
 
 app = FastAPI(debug=True)
 thread_pool = ThreadPoolExecutor(max_workers=30)
+
+
+class HtmlReqBody(BaseModel):
+    html: str
 
 
 @app.post(
     "/parse_html", 
     description="In this endpoint you should but only html content of the interested page"
 )
-async def parse_html(request: Request):
+async def parse_html(request: Request, req_body: HtmlReqBody):
+    if request.headers.get('auth') != get_token():
+        return {"ok": False, "message": "authentification required"}
+    
     message = "error when parsing html. give me a valid html string"
     loop = asyncio.get_running_loop()
     try:
-        html = (await request.body()).decode("utf-8")
+        html = req_body.html
         
         parser = Parser(html)
         message = "parser error. html is valid"
--- tests/test_api.py
+++ tests/test_api.py
@@ -4,16 +4,23 @@ import requests
 
 def test_api():
     url = "http://34.175.153.160.host.secureserver.net:7236/parse_html"
+    url = "http://127.0.0.1:3645/parse_html"
     
     test_html_dir = "./htmls_for_tests"
     for path in os.listdir(test_html_dir):
+        if path[0].islower():
+            continue
         if not path.endswith(".html"):
             continue
         
         with open(os.path.join(test_html_dir, path)) as f:
-            data =  f.read()
+            data = {"html": f.read()}
             
-        response = requests.post(url, data=data)
+        response = requests.post(
+            url, 
+            json=data,
+            headers={"Auth": os.environ["token"]}
+        )
         json = response.json()
         assert response.status_code == 200 and json["ok"] is True
         
